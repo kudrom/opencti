96bfbd94e37665274e5b02a123d38d70150a3ac1
Souad Hadjiat <souad.hadjiat@filigran.io>
2023-12-28 15:44:02 +0100

    [backend] add DecayRule type and built-in rules

--- opencti-platform/opencti-graphql/src/modules/indicator/decay-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/indicator/decay-domain.ts
@@ -1,32 +1,61 @@
-export interface DecayModel {
+export interface DecayRule {
   id: string
-  decay_lifetime: number
-  decay_factor: number
-  decay_pound: number
-  decay_points: number[]
-  decay_revoked_cutoff: number
+  decay_lifetime: number // in days
+  decay_pound: number // can be changed in other model when feature is ready.
+  decay_points: number[] // reactions points
+  decay_revoked_cutoff: number // revoked when score is <= 20
+  indicator_types: string[] // x_opencti_main_observable_type
+  order: number // low priority = 0
+  enabled: boolean
 }
 /** This is the model used when no configured decay model matches for an Indicator.
  * It's also the default one if nothing is configured yet. */
-export const FALLBACK_DECAY_MODEL: DecayModel = {
-  id: 'FALLBACK_DECAY_MODEL',
+export const FALLBACK_DECAY_RULE: DecayRule = {
+  id: 'FALLBACK_DECAY_RULE',
   decay_lifetime: 365, // 1 year
-  decay_factor: 3.0, // hardcoded
-  decay_pound: 1, // can be changed in other model when feature is ready.
-  decay_points: [80, 60, 40], // reactions points
-  decay_revoked_cutoff: 20 // revoked when score is <= 20
+  decay_pound: 1,
+  decay_points: [80, 60, 40, 20],
+  decay_revoked_cutoff: 0,
+  indicator_types: [],
+  order: 0,
+  enabled: true,
 };
+export const IP_DECAY_RULE: DecayRule = {
+  id: 'IP_DECAY_RULE',
+  decay_lifetime: 60,
+  decay_pound: 1,
+  decay_points: [80, 60, 40, 20],
+  decay_revoked_cutoff: 0,
+  indicator_types: ['IPv4-Addr', 'IPv6-Addr'],
+  order: 1,
+  enabled: true,
+};
+export const URL_DECAY_RULE: DecayRule = {
+  id: 'URL_DECAY_RULE',
+  decay_lifetime: 180,
+  decay_pound: 1,
+  decay_points: [80, 60, 40, 20],
+  decay_revoked_cutoff: 0,
+  indicator_types: ['Url'],
+  order: 1,
+  enabled: true,
+};
+export const BUILT_IN_DECAY_RULES = [
+  IP_DECAY_RULE, URL_DECAY_RULE, FALLBACK_DECAY_RULE,
+];
+const DECAY_FACTOR: number = 3.0;
+
 /**
  * Calculate the indicator score at a time (in days).
  * @param initialScore initial indicator score, usually between 100 and 0.
  * @param daysFromStart elapsed time in days since the start point.
- * @param model decay configuration to use.
+ * @param rule decay configuration to use.
  */
-export const computeScoreFromExpectedTime = (initialScore: number, daysFromStart: number, model: DecayModel) => {
+export const computeScoreFromExpectedTime = (initialScore: number, daysFromStart: number, rule: DecayRule) => {
   // Polynomial implementation (MISP approach)
-  if (daysFromStart > model.decay_lifetime) return 0;
+  if (daysFromStart > rule.decay_lifetime) return 0;
   if (daysFromStart <= 0) return initialScore;
-  return initialScore * (1 - ((daysFromStart / model.decay_lifetime) ** (1 / (model.decay_factor * model.decay_pound))));
+  return initialScore * (1 - ((daysFromStart / rule.decay_lifetime) ** (1 / (DECAY_FACTOR * rule.decay_pound))));
 };
 
 /**
@@ -35,7 +64,7 @@ export const computeScoreFromExpectedTime = (initialScore: number, daysFromStart
  * @param score the score value requested to calculate time
  * @param model decay configuration to use.
  */
-export const computeTimeFromExpectedScore = (initialScore: number, score: number, model: DecayModel) => {
+export const computeTimeFromExpectedScore = (initialScore: number, score: number, model: DecayRule) => {
   // Polynomial implementation (MISP approach)
-  return (Math.E ** (Math.log(1 - (score / initialScore)) * (model.decay_factor * model.decay_pound))) * model.decay_lifetime;
+  return (Math.E ** (Math.log(1 - (score / initialScore)) * (DECAY_FACTOR * model.decay_pound))) * model.decay_lifetime;
 };
--- opencti-platform/opencti-graphql/tests/01-unit/domain/decay-test.ts
+++ opencti-platform/opencti-graphql/tests/01-unit/domain/decay-test.ts
@@ -1,24 +1,24 @@
 import { describe, it, expect } from 'vitest';
-import { computeScoreFromExpectedTime, computeTimeFromExpectedScore, FALLBACK_DECAY_MODEL } from '../../../src/modules/indicator/decay-domain';
+import { computeScoreFromExpectedTime, computeTimeFromExpectedScore, FALLBACK_DECAY_RULE } from '../../../src/modules/indicator/decay-domain';
 
 describe('Decay formula testing', () => {
   it('should compute score', () => {
     const baseScore = 100;
-    const compute20Score = computeScoreFromExpectedTime(baseScore, 20, FALLBACK_DECAY_MODEL);
+    const compute20Score = computeScoreFromExpectedTime(baseScore, 20, FALLBACK_DECAY_RULE);
     expect(Math.round(compute20Score)).toBe(62);
-    const compute28Score = computeScoreFromExpectedTime(baseScore, 28, FALLBACK_DECAY_MODEL);
+    const compute28Score = computeScoreFromExpectedTime(baseScore, 28, FALLBACK_DECAY_RULE);
     expect(Math.round(compute28Score)).toBe(58);
-    const compute31Score = computeScoreFromExpectedTime(baseScore, 31, FALLBACK_DECAY_MODEL);
+    const compute31Score = computeScoreFromExpectedTime(baseScore, 31, FALLBACK_DECAY_RULE);
     expect(Math.round(compute31Score)).toBe(56);
-    const compute366Score = computeScoreFromExpectedTime(baseScore, 366, FALLBACK_DECAY_MODEL);
+    const compute366Score = computeScoreFromExpectedTime(baseScore, 366, FALLBACK_DECAY_RULE);
     expect(Math.round(compute366Score)).toBe(0);
-    const computeBadScore = computeScoreFromExpectedTime(baseScore, -5, FALLBACK_DECAY_MODEL);
+    const computeBadScore = computeScoreFromExpectedTime(baseScore, -5, FALLBACK_DECAY_RULE);
     expect(Math.round(computeBadScore)).toBe(100);
   });
 
   it('should compute score and time', () => {
     const baseScore = 100;
-    const compute20Score = computeScoreFromExpectedTime(baseScore, 20, FALLBACK_DECAY_MODEL);
-    expect(Math.round(computeTimeFromExpectedScore(baseScore, compute20Score, FALLBACK_DECAY_MODEL))).toBe(20);
+    const compute20Score = computeScoreFromExpectedTime(baseScore, 20, FALLBACK_DECAY_RULE);
+    expect(Math.round(computeTimeFromExpectedScore(baseScore, compute20Score, FALLBACK_DECAY_RULE))).toBe(20);
   });
 });
